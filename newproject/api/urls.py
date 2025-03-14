fc5e6f9154cbc9e56d88af56b38dec87e261dc4d
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-03-15 02:55:49 +0530

    added some improvements

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_users, create_user, get_users
+from .views import get_users, create_user, get_users, user_detail
 
 
 urlpatterns = [
 
     path('user/', get_users, name='get_users'),
-     path('user/create', create_user, name='create_user'),
+    path('user/create', create_user, name='create_user'),
+    path('user/<int:pk>', user_detail, name='get_user')
 ]
--- newproject/api/views.py
+++ newproject/api/views.py
@@ -21,4 +21,24 @@ def create_user(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
- 
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerialiazer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerialiazer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
