eb82dfe6899f0e4a299c0f90bb9ad0f6a3ff47f0
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-03-15 02:16:17 +0530

    created a API(post) and then add a url user/create

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -11,4 +11,12 @@ from .serialiazer import UserSerialiazer
 def get_user(request):  
     return Response(UserSerialiazer({'name' : 'shakeel','age': 23}).data)
 
+@api_view(['POST'])
+def create_user(request):  
+    serializer=UserSerialiazer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
  
